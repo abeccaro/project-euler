fb69fbd82f1d767cf16152653d2edf4997de7827
abeccaro <alex.becks@hotmail.it>
2018-01-04 13:29:30 +0100

    Solved problem 38

---
+++ Problems/Problem38/Problem38.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Alex Beccaro on 04/01/18.
+//
+
+#include <iostream>
+#include <vector>
+#include <generics.hpp>
+
+using namespace std;
+using namespace chrono;
+using timer = high_resolution_clock;
+using namespace generics;
+
+using numtype = unsigned long;
+
+/**
+ * Specifies values, solves problem and outputs solution and calculation time.
+ * @return The exit code
+ */
+int main() {
+    timer::time_point start = timer::now();
+
+    numtype max = 0;
+    const vector<unsigned short> all_digits = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (numtype i = 1; i < 10000; i++) {
+        vector<unsigned short> product_digits;
+
+        for (numtype j = 1; product_digits.size() < 9; j++) {
+            vector<unsigned short> partial_product = digits(i * j);
+
+            product_digits.insert(product_digits.end(), partial_product.begin(), partial_product.end());
+        }
+
+        numtype product = from_digits<numtype>(product_digits);
+        sort(product_digits.begin(), product_digits.end());
+
+        if (product_digits == all_digits && product > max)
+            max = product;
+    }
+
+    double time = duration_cast<microseconds>(timer::now() - start).count() / 1000000.0;
+
+    cout << "The largest 1 to 9 pandigital number that can be formed as the concatenated product of an integer with "
+         << "(1,2, ... , n) where n > 1 is " << max << endl;
+    cout << "Calculation took " << time << " seconds" << endl;
+}
