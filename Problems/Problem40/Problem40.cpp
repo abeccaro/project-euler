a7ef18df68b4b0cd31c79dc070e6c38ac6844d9a
abeccaro <alex.becks@hotmail.it>
2018-01-05 10:01:42 +0100

    Solved problem 40

---
+++ Problems/Problem40/Problem40.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Alex Beccaro on 05/01/18.
+//
+
+#include <iostream>
+#include <vector>
+#include <generics.hpp>
+
+using namespace std;
+using namespace chrono;
+using timer = high_resolution_clock;
+using generics::digits;
+
+using numtype = unsigned short;
+
+/**
+ * Specifies values, solves problem and outputs solution and calculation time.
+ * @return The exit code
+ */
+int main() {
+    timer::time_point start = timer::now();
+
+    vector<unsigned short> d;
+
+    for (numtype i = 1; d.size() < 1000000; i++) {
+        vector<unsigned short> digs = digits(i);
+        d.insert(d.end(), digs.begin(), digs.end());
+    }
+
+    numtype result = d[0] * d[9] * d[99] * d[999] * d[9999] * d[99999] * d[999999];
+
+    double time = duration_cast<microseconds>(timer::now() - start).count() / 1000000.0;
+
+    cout << "The product of requested digits is " << result << endl;
+    cout << "Calculation took " << time << " seconds" << endl;
+}
