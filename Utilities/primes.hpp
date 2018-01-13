1e03d6b228eb1e108239a3a2acf162691725f28a
abeccaro <alex.becks@hotmail.it>
2018-01-13 19:36:58 +0100

    Improved primality check using Miller-Rabin

--- Utilities/primes.hpp
+++ Utilities/primes.hpp
@@ -11,6 +11,70 @@
 using std::vector;
 using namespace template_conditions;
 using generics::divisors;
+using namespace boost::multiprecision;
+
+
+///region miller-rabin implementation
+
+/**
+ * Calculates (base ^ exponent) mod modulo
+ * @param base The base
+ * @param exponent The exponent
+ * @param modulo The modulo
+ * @return The result
+ */
+template<class T, class = typename enable_if<is_any_integral<T>::value>::type>
+T modular_power(T base, T exponent, T modulo) {
+    base %= modulo;
+    T result = 1;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * base) % modulo;
+        base = (base * base) % modulo;
+        exponent /= 2;
+    }
+
+    return result;
+}
+
+/**
+ * Miller-Rabin determinist algorithm for primality check
+ * @param n The number to check
+ * @param witnesses The witnesses values to check
+ * @return true if n is prime (using given witnesses), false otherwise
+ */
+template<class T, class = typename enable_if<is_any_integral<T>::value>::type>
+bool miller_rabin(const T& n, const vector<T>& witnesses) {
+    unsigned int s = 0;
+    T d = n - 1;
+    while (d % 2 == 0) {
+        s++;
+        d /= 2;
+    }
+//    auto l = log((cpp_dec_float_100) n);
+//    T max_witness = min<T>(n-1, (T) (2 * (l * l)));
+
+    for (const auto& a : witnesses) {
+        if (modular_power<T>(a, d, n) == 1)
+            continue;
+
+        bool composite = true;
+        for (unsigned int r = 0; r < s; r++) {
+            if (modular_power<T>(a, (T) pow(2, r) * d, n) == n - 1) {
+                composite = false;
+                break;
+            }
+        }
+
+        if (composite)
+            return false;
+    }
+
+    return true;
+}
+
+///endregion
 
 namespace primes {
     /**
@@ -91,13 +155,25 @@ namespace primes {
     }
 
     /**
-     * Checks if given number is prime.
+     * Checks if given number is prime. Correct at least for any number up to 2^64.
      * @param n The number to check
      * @return True if given number is prime, false otherwise
      */
     template<class T, class = typename enable_if<is_any_integral<T>::value>::type>
     bool is_prime(const T& n) {
-        return divisors(n).size() == 2;
+        if (n < 2 || (n % 2 == 0 && n != 2) || (n % 3 == 0 && n != 3))
+            return false;
+
+        if (n <= 3)
+            return true;
+
+        if (n < 1373653)
+            return miller_rabin(n, {2, 3});
+        if (n < 4759123141)
+            return miller_rabin(n, {2, 7, 61});
+        if (n < 3071837692357849)
+            return miller_rabin(n, {2, 75088, 642735, 203659041, 3613982119});
+        return miller_rabin(n, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
     }
 }
 
