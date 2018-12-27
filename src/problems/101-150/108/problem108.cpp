024d2d31100c9b7203fc8d215deff8d2991e0b2a
Alex Beccaro <alex.becks@hotmail.it>
2018-12-27 13:49:01 +0100

    Improved problem 108 solution

--- src/problems/101-150/108/problem108.cpp
+++ src/problems/101-150/108/problem108.cpp
@@ -3,25 +3,38 @@
 //
 
 #include "problem108.hpp"
-#import <primes.hpp>
 
-using std::vector;
-
-using primes::prime_factors_multeplicity;
+using series::primes;
+using prime_it = primes<uint32_t>::iterator;
 
 namespace problems {
-    uint32_t problem108::solve(uint32_t n) {
-        uint32_t step = 6; // empirical
-        for (uint64_t i = step; ; i += step) {
-            auto pf = prime_factors_multeplicity(i);
+    uint32_t problem108::find_num(uint32_t solutions, uint32_t best, const prime_it& p, uint32_t max_mult_p,
+            uint32_t n, uint32_t sol_found) {
+        if (n > best)
+            return best;
 
-            uint32_t solutions = 1;
-            for (const auto& p : pf)
-                solutions *= 2 * p.second + 1;
-            solutions = (solutions + 1) / 2;
-
-            if (solutions > n)
-                return i;
+        if (sol_found > solutions) {
+            best = n;
+            return best;
         }
+
+        for (uint32_t i = 1; i <= max_mult_p && n < best; i++) {
+            n *= *p;
+            uint32_t new_solutions = sol_found * (i * 2 + 1);
+
+            uint32_t rec_best = find_num(solutions, best, p + 1, i, n, new_solutions);
+
+            if (rec_best < best)
+                best = rec_best;
+        }
+
+        return best;
+    }
+
+    uint64_t problem108::solve(uint32_t n) {
+        primes<uint32_t> ps;
+        uint32_t best = std::numeric_limits<uint32_t>::max();
+
+        return find_num(2 * n, best, ps.begin(), n, 1, 1);
     }
 }
--- src/problems/101-150/108/problem108.hpp
+++ src/problems/101-150/108/problem108.hpp
@@ -6,16 +6,31 @@
 #define PROJECT_EULER_PROBLEM108_HPP
 
 #include <cstdint>
-#include <vector>
+#include <series/primes.hpp>
 
 namespace problems {
     class problem108 {
+    private:
+        /**
+         * Finds recursively the lowest number n to have at least 'solutions' distinct
+         * solutions to 1/x + 1/y = 1/n.
+         *
+         * @param solutions The lower bound of solutions to find
+         * @param best The best n found so far
+         * @param p The next prime factor to consider
+         * @param max_mult_p The maximum multeplicity of p to consider
+         * @param n Current number
+         * @param sol_found Current number of solutions found
+         * @return The lowest number n to have at least 'solutions' distinct solutions.
+         */
+        static uint32_t find_num(uint32_t solutions, uint32_t best, const series::primes<uint32_t>::iterator& p,
+                                 uint32_t max_mult_p, uint32_t n, uint32_t sol_found);
     public:
         /**
          * Solves problem
          * @return The solution
          */
-        static uint32_t solve(uint32_t n = 1000);
+        static uint64_t solve(uint32_t n = 1000);
     };
 }
 
